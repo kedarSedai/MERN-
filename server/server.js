6df2fac3e4285222140a5bb19aa54ecc83df4674
kedar sedai <kedar.sedai05@gmail.com>
2021-05-11 19:48:13 +0545

    Added DB connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+require('dotenv').config()
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.json());
+
+//DATABASE CONNECTION
+mongoose
+    .connect(process.env.DB, ({useCreateIndex: true, useUnifiedTopology:true, useNewUrlParser:true}))
+    .then(() => console.log("Database Connected !!!"))
+    .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
 

f8c3add26441c1742bd9e6a0270c061695be3479
kedar sedai <kedar.sedai05@gmail.com>
2021-05-12 20:50:22 +0545

    Changes in Controller Logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,21 @@
+import PostMessage from '../models/postMessage.js';
 
-export const getPosts = (req, res) =>{
-    res.send('This works');
+export const getPosts = async (req, res) =>{
+    try {
+        const postMessage = await PostMessage.find();
+        res.status(200).json(postMessage);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }
-export const createPost = (req, res) => {
-    res.send("This is post routes")
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(200).json(newPost);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }

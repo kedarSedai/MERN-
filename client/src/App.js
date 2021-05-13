5c11ab663e0f749e7814bebdcc930df3c41ef20a
kedar sedai <kedar.sedai05@gmail.com>
2021-05-13 10:39:04 +0545

    Added material UI and API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import react from 'react';
+import React from 'react';
+import { Container, AppBar, Typography , Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+import useStyles from './styles';
 
-const App = () => {
-    return(
-        <div>
-           <h1>App</h1> 
-        </div>
-    )
+
+const App = () =>{
+    const classes = useStyles();
+    return (
+        <Container maxwidth="lg">
+            <AppBar className={classes.AppBar} position="static" color="inherit">
+                <Typography className={classes.heading} varient="h2" align="center">Memories
+                <img src={memories} className={classes.image} alt="logo" height="60" />
+                </Typography >
+                
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
